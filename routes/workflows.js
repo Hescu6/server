01767cb23a91ba4ab901da9975d34dbcd6d0efa3
Hil <hildebrando.escudero@udc.edu>
2019-12-31 16:03:47 -0500

    .

--- routes/workflows.js
+++ routes/workflows.js
@@ -17,47 +17,47 @@ const userRoutes = (app, fs) => {
         });
     };
 
-    // const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
+    const writeFile = (fileData, callback, filePath = dataPath, encoding = 'utf8') => {
 
-    //     fs.writeFile(filePath, fileData, encoding, (err) => {
-    //         if (err) {
-    //             throw err;
-    //         }
+        fs.writeFile(filePath, fileData, encoding, (err) => {
+            if (err) {
+                throw err;
+            }
 
-    //         callback();
-    //     });
-    // };
+            callback();
+        });
+    };
 
-    // // UPDATE
-    // app.put('/workflows/:name', (req, res) => {
+    // UPDATE
+    app.put('/workflows/:name', (req, res) => {
 
-    //     readFile(data => {
+        readFile(data => {
 
-    //         // add the new user
-    //         const name = req.params["name"];
-    //         index = 
-    //         data['workflows'] = JSON.parse(req.body.data);
+            // add the new user
+            const name = req.params["name"];
+            index = 
+            data['workflows'] = JSON.parse(req.body.data);
 
-    //         writeFile(JSON.stringify(data, null, 2), () => {
-    //             res.status(200).send(`users id:${userId} updated`);
-    //         });
-    //     },
-    //         true);
-    // });
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send(`users id:${userId} updated`);
+            });
+        },
+            true);
+    });
 
-    // //WRITE
-    // app.post('/workflows', (req, res) => {
+    //WRITE
+    app.post('/workflows', (req, res) => {
 
-    //     readFile(data => {
-    //         console.log(data['workflows'])
-    //         // add the new user
-    //         data['workflows'].push(req.body);
-    //         writeFile(JSON.stringify(data, null, 2), () => {
-    //             res.status(200).send('new workflow added');
-    //         });
-    //     },
-    //         true);
-    // });
+        readFile(data => {
+            console.log(data['workflows'])
+            // add the new user
+            data['workflows'].push(req.body);
+            writeFile(JSON.stringify(data, null, 2), () => {
+                res.status(200).send('new workflow added');
+            });
+        },
+            true);
+    });
 
 
 
