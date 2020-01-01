d416196f8c1712c6471b47491ea4ebe23272b0ac
Hil <hildebrando.escudero@udc.edu>
2020-01-01 12:37:15 -0500

    /

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cors(
 }
 ));
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static('docs'))
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
